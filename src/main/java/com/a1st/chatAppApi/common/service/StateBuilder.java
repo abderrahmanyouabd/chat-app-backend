a09188f092a85f873e635bc15aba7940226e9e74
Abderrahman Youabd <youabd50@gmail.com>
2024-07-07 12:47:34 +0200

    add: added common custom State services

---
+++ src/main/java/com/a1st/chatAppApi/common/service/StateBuilder.java
@@ -0,0 +1,33 @@
+package com.a1st.chatAppApi.common.service;
+
+/**
+ * @author: Abderrahman Youabd aka: A1ST
+ * @version: 1.0
+ */
+public class StateBuilder<T, V> {
+    private StatusNotification status;
+    private T value;
+    private V error;
+
+    public State<T, V> forError(V error) {
+        this.error = error;
+        this.status = StatusNotification.ERROR;
+        return new State<>(this.status, this.value, this.error);
+    }
+
+    public State<T, V> forSuccess() {
+        this.status = StatusNotification.OK;
+        return new State<>(this.status, this.value, this.error);
+    }
+
+    public State<T, V> forSuccess(T value) {
+        this.value = value;
+        this.status = StatusNotification.OK;
+        return new State<>(this.status, this.value, this.error);
+    }
+    public State<T, V> forUnauthorized(V error) {
+        this.error = error;
+        this.status = StatusNotification.UNAUTHORIZED;
+        return new State<>(this.status, this.value, this.error);
+    }
+}
---
+++ src/main/java/com/a1st/chatAppApi/common/service/StatusNotification.java
@@ -0,0 +1,9 @@
+package com.a1st.chatAppApi.common.service;
+
+/**
+ * @author: Abderrahman Youabd aka: A1ST
+ * @version: 1.0
+ */
+public enum StatusNotification {
+    OK, ERROR, UNAUTHORIZED;
+}
