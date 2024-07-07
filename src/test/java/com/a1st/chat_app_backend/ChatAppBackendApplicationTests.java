761944e652eabbe50b5d23c4973a81339de0d3d9
Abderrahman Youabd <youabd50@gmail.com>
2024-07-07 12:40:32 +0200

    Initial commit

---
+++ src/test/java/com/a1st/chat_app_backend/ChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a1st.chat_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
